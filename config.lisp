353657bde50de6a5242611b3d4cbd7ecf3af78cf
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-04-18 19:43:24 -0700

    enabling http-var in config

--- config.lisp
+++ config.lisp
@@ -28,7 +28,7 @@
   "Boolean specifying whether or not to hide the Wookie version in the Server
    header.")
 
-(defvar *enabled-plugins* '(:get :post :multipart :cookie :directory-router)
+(defvar *enabled-plugins* '(:get :post :multipart :http-var :cookie :directory-router)
   "A list of (keyword) names of enabled plugins.")
 
 (defvar *tmp-file-store* (asdf:system-relative-pathname :wookie #p"upload-tmp/")
