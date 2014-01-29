24825c68756077c24e6758f5e99ed99585a3fa47
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-01-29 00:22:10 -0800

    only forcing chunking on chunked routes if the client didnt specify transfer-encoding: chunked. eventually would like to make it a router option...

--- parser.lisp
+++ parser.lisp
@@ -171,9 +171,10 @@
                        ;; as one big chunk.
                        (when (and found-route
                                   (getf found-route :allow-chunking))
-                         ;; support large uploads from idiot browsers a bit
-                         ;; better (no streaming uploads in XHR)
-                         (setf (http-parse:http-force-stream http) t)
+                         (unless (string= (string-downcase (getf headers :transfer-encoding)) "chunked")
+                           ;; support large uploads from idiot browsers a bit
+                           ;; better (no streaming uploads in XHR)
+                           (setf (http-parse:http-force-stream http) t))
                          (dispatch-route)))))
                  ;; pipe all uncaught errors we get to the main event handler
                  ;; (with our socket object).
