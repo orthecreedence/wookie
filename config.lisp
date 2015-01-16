b5f71212895e46ae17a96ba1401c74c81e2c092b
Andrew Danger Lyon <orthecreedence@gmail.com>
2015-01-15 22:30:28 -0800

    adding *max-body-size*, which tells wookie to send an error to the client if the body exceeds a certain limit (in bytes)

--- config.lisp
+++ config.lisp
@@ -1,6 +1,7 @@
 (defpackage :wookie-config
   (:use :cl)
   (:export #:*debug-on-error*
+           #:*max-body-size*
            #:*hide-version*
            #:*enabled-plugins*
            #:*tmp-file-store*))
@@ -8,6 +9,10 @@
 
 (defvar *debug-on-error* nil
   "If T, will not catch errors but instead allow them to go to the debugger.")
+
+(defvar *max-body-size* (* 1024 1024 2)
+  "The max HTTP body size in bytes that Wookie will store for each request,
+   assuming the request is set to store the body (see request-store-body).")
 
 (defvar *hide-version* nil
   "Boolean specifying whether or not to hide the Wookie version in the Server
